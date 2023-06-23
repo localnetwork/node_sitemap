442ef812d20753738c0cafd1716ced9ef84e95db
localnetwork <diomenike@gmail.com>
2023-06-23 11:24:47 +0800

    added filter in sitemap per content type

--- src/Controller/NodeSitemapController.php
+++ src/Controller/NodeSitemapController.php
@@ -7,62 +7,89 @@ use Symfony\Component\HttpFoundation\Response;
 use Drupal\Core\Url;
 use Drupal\node\Entity\Node;
 use Drupal\Core\Database\Database;
-
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class NodeSitemapController extends ControllerBase {
-    
 
-    public function sitemapXml() {
-        $current_language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+  /**
+   * The configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
 
-        $config = \Drupal::config('node_sitemap.content_type_config');
-        $data = $config->get();
-        $content_types = \Drupal::entityTypeManager()->getStorage('node_type')->loadMultiple();
-        $pager_limit = $data['pager_limit'];
-        $translated_nodes = [];
+  /**
+   * Constructs a NodeSitemapController object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
 
-        foreach($content_types as $type) {
-            if($data[$type->id()] === 1) {
-                $types[$type->id()] = $type->id();
-            }
-            
-        }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory')
+    );
+  }
 
-//        $nodes = \Drupal::entityTypeManager()
-//      ->getStorage('node')
-//      ->loadByProperties(['type' => $types, 'status' => 1]);
-        $query = \Drupal::entityQuery('node')
-          ->condition('type', $types, 'IN')
-          ->condition('status', 1)
-          ->sort('changed', 'DESC')
-          ->pager($pager_limit);
-        $nids = $query->execute();
-        $nodes = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple($nids);
+  public function sitemapXml(Request $request) {
+    $current_language = \Drupal::languageManager()->getCurrentLanguage()->getId();
 
-        foreach ($nodes as $node) {
-          $translations = $node->getTranslationLanguages();
-          if (isset($translations[$current_language])) {
-            $url = Url::fromRoute('entity.node.canonical', ['node' => $node->id()], ['absolute' => TRUE]);
-
-
-
-            $translated_nodes[] = array(
-                'nodeinfo' => $node,
-                'id' => $node->id(),
-                'priority' => $data['priority_'. $node->type->target_id], 
-            );
-          }
-        }
-
-        $build = [
-            '#theme' => 'sitemap_xml',
-            '#nodes' => $translated_nodes,
-        ];
-
-        // return $build;  
-        $response = new Response(render($build));
-        $response->headers->set('Content-Type', 'application/xml');
-        return $response;
+    $config = $this->configFactory->get('node_sitemap.content_type_config');
+    if ($config === NULL) {
+      throw new \Exception('Unable to load node_sitemap.content_type_config configuration.');
     }
+    $data = $config->get();
+    $content_types = \Drupal::entityTypeManager()->getStorage('node_type')->loadMultiple();
+    $pager_limit = $data['pager_limit'];
+    $translated_nodes = [];
+
+    $typeFilter = $request->query->get('type'); // Get the 'type' parameter from the URL query
+
+    foreach ($content_types as $type) {
+      if ($typeFilter && $typeFilter !== $type->id()) {
+        continue; // Skip this content type if it doesn't match the filter
+      }
+      if ($data[$type->id()] === 1) {
+        $types[$type->id()] = $type->id();
+      }
+    }
+
+    $query = \Drupal::entityQuery('node')
+      ->condition('type', $types, 'IN')
+      ->condition('status', 1)
+      ->sort('changed', 'DESC')
+      ->pager($pager_limit);
+    $nids = $query->execute();
+    $nodes = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple($nids);
+
+    foreach ($nodes as $node) {
+      $translations = $node->getTranslationLanguages();
+      if (isset($translations[$current_language])) {
+        $url = Url::fromRoute('entity.node.canonical', ['node' => $node->id()], ['absolute' => TRUE]);
+
+        $translated_nodes[] = [
+          'nodeinfo' => $node,
+          'id' => $node->id(),
+          'priority' => $data['priority_'. $node->type->target_id], 
+        ];
+      }
+    }
+
+    $build = [
+      '#theme' => 'sitemap_xml',
+      '#nodes' => $translated_nodes,
+    ];
+
+    $response = new Response(render($build));
+    $response->headers->set('Content-Type', 'application/xml');
+    return $response;
+  }
 }
- 
